d9703f1aca38114073d2001feb64db743baddecf
Bae-Jin-Young <bae20051018@gmail.com>
2021-10-15 22:18:26 +0900

    Study : Movie App finish

--- src/comtonents/Movie.js
+++ src/comtonents/Movie.js
@@ -1,24 +1,38 @@
 import React from "react";
 import PropTypes from "prop-types";
 import "./Movie.css";
+import { Link } from "react-router-dom";
 
-function Movie({ year, title, summary, poster, genres }) {
+function Movie({ id, year, title, summary, poster, genres }) {
   return (
-    <div class="movie">
-      <img src={poster} alt={title} title={title}></img>
-      <div className="movie_data">
-        <h3 className="movie_title">{title}</h3>
-        <h5 className="movie_year">{year}</h5>
-        <ul className="movie_genres">
-          {genres.map((genre, index) => (
-            <li key={index} className="genres_genre">
-              {genre}
-            </li>
-          ))}
-        </ul>
-        <p className="movie_summary">{summary.slice(0, 180)}</p>
+    <Link
+      to={{
+        pathname: `/movie/${id}`,
+        state: {
+          year,
+          title,
+          summary,
+          poster,
+          genres,
+        },
+      }}
+    >
+      <div className="movie">
+        <img src={poster} alt={title} title={title}></img>
+        <div className="movie_data">
+          <h3 className="movie_title">{title}</h3>
+          <h5 className="movie_year">{year}</h5>
+          <ul className="movie_genres">
+            {genres.map((genre, index) => (
+              <li key={index} className="genres_genre">
+                {genre}
+              </li>
+            ))}
+          </ul>
+          <p className="movie_summary">{summary.slice(0, 180)}</p>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 }
 
@@ -28,7 +42,7 @@ Movie.propTypes = {
   title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
   poster: PropTypes.string.isRequired,
-  generes: PropTypes.arrayOf(PropTypes.string).isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
 
 export default Movie;
